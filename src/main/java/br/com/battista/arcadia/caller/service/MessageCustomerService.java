3542f1d5ab5a3c304634dc74d5dfcdebbe842662
Rafael Batista <rabsouza@gmail.com>
2016-07-17 13:07:11 -0300

    [rabsouza] Correcoes sonar e cobertura de teste

--- src/main/java/br/com/battista/arcadia/caller/service/MessageCustomerService.java
+++ src/main/java/br/com/battista/arcadia/caller/service/MessageCustomerService.java
@@ -3,7 +3,6 @@ package br.com.battista.arcadia.caller.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
-import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +18,7 @@ public class MessageCustomerService implements MessageSourceAware {
         this.messageSource = messageSource;
     }
 
-    public String getMessage(String s, Object... objects) throws NoSuchMessageException {
+    public String getMessage(String s, Object... objects) {
         return messageSource.getMessage(s, objects, LocaleContextHolder.getLocale());
     }
 }
